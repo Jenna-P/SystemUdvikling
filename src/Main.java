b54a4e22561845381d6cde9e6ceaee6475657863
jenna-P <jenn2464@edu.easj.dk>
2020-09-21 14:26:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+            System.out.print("Enter three numbers as integer : ");
+            int x = input.nextInt();
+            int y = input.nextInt();
+            int z = input.nextInt();
+
+            boolean a = Userinput.metode1(x,y,z) ;
+
+            System.out.println(a);
+
+
+
+        }
+    }
+
---
+++ src/Userinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Userinput {
+
+    public static boolean metode1(int x, int y, int z) {
+        boolean a = !(x != y - z) ;
+        return  a;
+
+
+
+    }
+    }
+
